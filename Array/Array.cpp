e40e013ecf6bf3d7ba6bf6770657ada382531496
Dallin Bunnell <dallinbunnellswims@hotmail.com>
2016-02-18 09:20:30 -0700

    Initial commit

---
+++ Array/Array.cpp
@@ -0,0 +1,25 @@
+// Array.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+#include <iostream>
+#include <iostream>
+
+int size;
+std::vector<int> vector1;
+std::vector<int> vector2;
+
+int main()
+{
+	std::cout << "This program will have you make two vectors. The first one will fill automatically, the second you'll need to fill." << std::endl;
+	std::cout << "After the vectors are filled, I'll find the average and sum of each." << std::endl;
+	std::cout << "Please enter the size of the first vector.";
+	std::cin >> size;
+
+	
+
+	
+	return 0;
+}
+
